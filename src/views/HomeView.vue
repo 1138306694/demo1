dc64a037b01c48a6f37248b17c52a88d439a3c15
Liangzhe <1138306694@qq.com>
2022-08-08 14:40:23 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorldss msg="哈哈哈哈哈哈"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorldss from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorldss
+  }
+}
+</script>
