413ef3d7bc853cbf05ffc7fdc3377c5a2066a3dd
Liangzhe <1138306694@qq.com>
2023-03-23 15:59:00 +0800

    翻译

--- src/http.js
+++ src/http.js
@@ -13,9 +13,9 @@ console.log(axios.defaults, '......axios.......');
 //生产环境;  https://wechat.nxqlhhk.top/
 axios.defaults.baseURL = 'https://wechat.nxqlhhk.top/' 
 // 本地环境
-let ip = "localhost";
+// let ip = "localhost";
 // let ip = "192.168.31.32";
-axios.defaults.baseURL = 'http://'+ip+':1999/'
+// axios.defaults.baseURL = 'http://'+ip+':1999/'
 // axios.defaults.headers.common['token'] = "";
 
 // POST传参序列化(添加请求拦截器)
